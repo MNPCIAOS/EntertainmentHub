85d328f0f642741f1e1008c4b70eb22c8ad5bc7a
MNPCIAOS <princechristianmbabazi@example.com>
2026-09-10 00:38:00 +0200

    Fix admin creation to prevent build failure

--- build.sh
+++ build.sh
@@ -2,8 +2,11 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Run database migrations
 python manage.py migrate
 
-# This runs your custom setup automatically using variables we will set in Render
-python manage.py setup_content_admin
+# Create the admin user automatically without asking questions
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'princechristianmbabazi@gmail.com', 'Prince@123#CHRISTIAN')"
